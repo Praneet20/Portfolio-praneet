078bf35bd420188b575b4cc27838ef2163887269
Ramandeep Singh <ramandeepsingh@Ramandeeps-MacBook-Pro-3.local>
2020-05-09 02:42:02 -0400

    added Particles

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,71 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Header from './Header';
+import Particles from 'react-particles-js'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+particlesCanva : {
+    position: "absolute",
+    left: 0
+}
+
+
+})
 
 const Home = () => {
-
+ const classes = useStyles()
 return (
-    <div>
+    <>
        <Navbar/>
        <Header/>
+       <Particles
+       canvasClassName={classes.particlesCanva}
+       params={{
+           particles:{
+               number: {
+                   value: 45,
+                  density:{
+                      enable: true,
+                      value_area: 900
+
+                  }
+               },
+               shape:{
+                   type:"star",
+                   stroke: {
+                       width: 1,
+                       color: "tomato"
+                   }
+               },
+
+               size: {
+                   value: 8,
+                   random : true,
+                   anim : {
+                       enable: true,
+                       speed: 10,
+                       size_min: 0.1,
+                       sync:true
+                   }
+               },
+               opacity: {
+                   value: 1,
+                   random: true,
+                   anim : {
+                    enable: true,
+                    speed: 1,
+                    opacity_min: 0
+                   }
+
+               }
+               
+           }
+       }}
+      
+       />
        
-    </div>
+    </>
 )
 
 }
